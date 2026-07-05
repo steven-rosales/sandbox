48a7042cb4b38d00736d46978057e870454717de
steven <yoascending@gmail.com>
2026-07-05 12:45:14 -0400

    readable streams

---
+++ streams/custom-readable/custom-readable.js
@@ -0,0 +1,46 @@
+import { Readable } from "node:stream";
+import fs from "node:fs";
+
+class FileReadStream extends Readable {
+  constructor({ highWaterMark, fileName }) {
+    super({ highWaterMark });
+    this.fileName = fileName;
+    this.fd = null;
+  }
+
+  _construct(callback) {
+    fs.open(this.fileName, "r", (err, fd) => {
+      if (err) return callback(err);
+      this.fd = fd;
+      callback();
+    });
+  }
+
+  _read(size) {
+    const buff = Buffer.alloc(size);
+    fs.read(this.fd, buff, 0, size, null, (err, bytesRead) => {
+      if (err) return this.destroy(err);
+      this.push(bytesRead > 0 ? buff.subarray(0, bytesRead) : null);
+    });
+  }
+
+  _destroy(error, callback) {
+    if (this.fd) {
+      fs.close(this.fd, (err) => callback(err || error));
+    } else {
+      callback(error);
+    }
+  }
+}
+
+const stream = new FileReadStream({
+  fileName: "./streams/custom-readable/text.txt",
+});
+
+stream.on("data", (chunk) => {
+  console.log(chunk.toString("utf-8"));
+});
+
+stream.on("end", () => {
+  console.log("Stream is done reading");
+});
